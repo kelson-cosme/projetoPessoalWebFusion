d71077a9ef82379737b221393027942a844cdd1e
kelson.almeida <kelson.almeida123@gmail.com>
2024-07-03 17:10:49 -0400

    att

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 
-import React, { useLayoutEffect, useReducer, useRef } from "react";
-import "./Home.css"
+
+import React, { useLayoutEffect, useRef } from "react";
+import "./Home.css";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
-import Fundo from "../../assets/fundo.jpg"
-import logoE from "../../assets/logoE.png"
-import logoD from "../../assets/logoD.png"
+import Fundo from "../../assets/fundo.jpg";
+import logoE from "../../assets/logoE.png";
+import logoD from "../../assets/logoD.png";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -14,7 +15,10 @@ function Home() {
   const sectionRef = useRef(null);
   const titleRef = useRef(null);
   const dotRef = useRef(null);
+  const timiline2 = useRef(null);
+  const elemento2 = useRef(null)
 
+  // Efeito Preencher
   useLayoutEffect(() => {
     const section = sectionRef.current;
     const title = titleRef.current;
@@ -77,50 +81,106 @@ function Home() {
     };
   }, []);
 
+  const timiline = useRef();
+  const elemento = useRef();
+
+  // Efeito Mexer imagens
+  useLayoutEffect(() => {
+    const cpx = gsap.context(() => {
+      timiline.current = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".imgs",
+          scrub: true,
+          // markers: true,
+          start: "top 20px",
+          end: "bottom 100vw",
+          rotate: "0deg"
+        }
+      })
+      .fromTo("#img1", {
+        opacity: 1,
+        x: 0,
+        rotate: "0deg"
+      }, {
+        opacity: 0,
+        x: -160,
+        rotate: "-60deg"
+      })
+      .fromTo("#img2", {
+        opacity: 1,
+        x: 0,
+        rotate: "0deg"
+      }, {
+        opacity: 0,
+        x: 160,
+        rotate: "-60deg"
+      });
+    });
+
+  }, [elemento]);
 
 
-    const timiline = useRef()
-    const elemento = useRef()
+
+
+
+
+  // Efeito Zoom
+  useLayoutEffect(() => {
+    gsap.registerEffect({
+      name: "zoom",
+      effect: (targets, config) => {
+        const vars = { transformOrigin: "0px 0px", ...config },
+          { scale, origin } = config,
+          clamp = gsap.utils.clamp(-100 * (scale - 1), 0);
+        delete vars.origin;
+        vars.xPercent = clamp((0.5 - origin[0] * scale) * 100);
+        vars.yPercent = clamp((0.5 - origin[1] * scale) * 100);
+        vars.overwrite = "auto";
+        return gsap.to(targets, vars);
+      },
+      extendTimeline: true,
+      defaults: { origin: [0.5, 0.5], scale: 2 },
+    });
+
+    const zoomData = [
+      { scale: 1, origin: [0.5, 0.5] },
+      { scale: 3, origin: [0.6, 1] },
+      { scale: 1, origin: [0.5, 0.5] },
+      { scale: 2, origin: [0.8, 0.4] },
+    ];
 
     
-    useLayoutEffect( () => {
-        const cpx = gsap.context( () => {
-          timiline.current = gsap.timeline({
-            scrollTrigger:{
-                trigger:".imgs",
-                scrub: true,
-                // markers: true,
-                start: "top 20px",
-                end: "bottom 100vw",
-                rotate: "0deg"
-            }
-          })
-          .fromTo("#img1", {
-            opacity: 1,
-            x: 0,
-            rotate: "0deg"
 
-          }, {
-            opacity: 0,
-            x: -160,
-            rotate: "-60deg"
+    gsap.utils.toArray(".zoomI").forEach((section, index) => {
+        const zoom = zoomData[index];
 
-          }) .fromTo("#img2", {
-            opacity: 1,
-            x: 0,
-            rotate: "0deg"
-        },{
-            opacity: 0,
-            x: 160,
-            rotate: "-60deg"
+            const cp = gsap.context( () => {
+                timiline2.current = gsap.timeline({
+                    scrollTrigger: {
+                        trigger: section,
+                        // markers: true,
+                        start: "top 85%",
+                        end: "+=125%",
+                        ontoggle(self){
+                            if(self.isActive){
+                                gsap.effects.zoom(".photo", {
+                                    scale: zoom.scale,
+                                    origin: zoom.origin,
+                                    duration: 1,
+                                    ease: "power1.inOut",
+                                })
+                            }
+                        }
+                    }
+                })
+            })
 
-          })
-        })
-
-    } , elemento)
+                });
 
 
-    
+
+
+        }, []);
 
 
   return (
@@ -174,20 +234,34 @@ function Home() {
           </div>
           
 
-        <div className="performace">
-          texto
+        <div ref={elemento2} className="performace">
+
+          <div  className="right-side">
+            <div className="photo"></div>
+          </div>
+
+          <section id="z1" className="zoomI blue">
+            <h1>ScrollTrigger Zoom by Section</h1>
+          </section>
+
+          <section id="z2" className="zoomI green">
+            <h1>Section 2</h1>
+          </section>
+
+          <section id="z3" className="zoomI white">
+            <h1>Section 3</h1>
+          </section>
+
+          <section id="z4" className="zoomI yellow">
+            <h1>Section 4</h1>
+          </section>
         </div>
 
-        <div className="teste">
-          <h1>testesteste ste</h1>
+
+       
         </div>
 
-        </div>
 
-        
-
-
-        
       </div>
 
       
